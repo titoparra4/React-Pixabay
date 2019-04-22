70daf93e08dd5f234d2f6d04c3c1918482838802
Tito <tito.parra4@@hotmail.com>
2019-04-22 08:45:32 +0200

    Reading the form values and adding the Value to the State

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import Buscador from './componentes/Buscador';
 
 class App extends Component {
+	state = {
+		termino: ''
+	};
+
+	consultarAPI = () => {
+		console.log('From API');
+	};
+
+	datosBusqueda = (termino) => {
+		this.setState(
+			{
+				termino
+			},
+			() => {
+				this.consultarAPI();
+			}
+		);
+	};
+
 	render() {
 		return (
 			<div className="App container">
 				<div className="jumbotron">
 					<p className="lead text-center">Image search engine</p>
-					<Buscador />
+					<Buscador datosBusqueda={this.datosBusqueda} />
 				</div>
 			</div>
 		);
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class Buscador extends Component {
+	busquedaRef = React.createRef();
+
+	obtenerDatos = (e) => {
+		e.preventDefault();
+
+		const termino = this.busquedaRef.current.value;
+
+		this.props.datosBusqueda(termino);
+	};
+
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.obtenerDatos}>
 				<div className="row">
 					<div className="form-group col-md-8">
 						<input
+							ref={this.busquedaRef}
 							className="form-control form-control-lg"
 							type="text"
 							placeholder="Look for your image, example: soccer"
